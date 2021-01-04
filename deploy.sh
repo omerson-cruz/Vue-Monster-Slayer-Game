c972136da894eb4609bba87f5926fd43f9748d8c
omerson-cruz <omerson.e.cruz@gmail.com>
2021-01-04 14:57:12 +0800

    Edited Config

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-git push -f git@github.com:omerson-cruz/monster-slayer.git master:gh-pages
+git push -f git@github.com:omerson-cruz/Vue-Monster-Slayer-Game.git master:gh-pages
 
 cd -
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath:
     process.env.NODE_ENV === "production"
-      ? "/monster-slayer/" // <== name of your GH repository
+      ? "/Vue-Monster-Slayer-Game/" // <== name of your GH repository
       : "/",
 };
